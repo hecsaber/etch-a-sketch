5232db386d35def96c4c2cf026966afa1f439a55
Csaba Hegyi <hegy.csab@gmail.com>
2020-09-07 11:16:54 +0200

    page reload after changing the size of the blocks

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
-const container = document.querySelector("#container");
+function createBlocks(sizeOfBlock) {
+    const container = document.querySelector("#container");
+    const width = document.getElementById("container").clientWidth;
 
-const width = document.getElementById("container").clientWidth;
-console.log(width);
+    const blocksToAdd = Math.floor(width / blockSize());
 
-
-const blocksToAdd = width / 16;
-
-// create the blocks
-for (i = 1; i <= blocksToAdd; i++) {
-    var rowContainer = document.createElement("div");
-    for (j = 1; j <= blocksToAdd; j++) {
-        var blocks = document.createElement("div");
-        blocks.style.backgroundColor = "white";
-        blocks.style.width = "14px";
-        blocks.style.height = "14px";
-        blocks.style.border = "solid 1px blueviolet";
-        blocks.classList.add("thisBlock")
-        rowContainer.appendChild(blocks);
+    // create the blocks
+    for (i = 1; i <= blocksToAdd; i++) {
+        var rowContainer = document.createElement("div");
+        for (j = 1; j <= blocksToAdd; j++) {
+            var blocks = document.createElement("div");
+            blocks.style.backgroundColor = "white";
+            blocks.style.width = `${sizeOfBlock-2}px`;
+            blocks.style.height = `${sizeOfBlock-2}px`;
+            blocks.style.border = "solid 1px grey";
+            blocks.classList.add("thisBlock")
+            rowContainer.appendChild(blocks);
+        }
+        container.appendChild(rowContainer);
     }
-    container.appendChild(rowContainer);
 }
+
+createBlocks(blockSize());
+
+var sizeChanger = document.getElementById("blocksize");
+
+sizeChanger.addEventListener("change", function() {
+    var sizeChange = blockSize();
+    location.reload();
+    createBlocks(sizeChange);
+})
+
 
 const divs = document.querySelectorAll(".thisBlock");
 
 divs.forEach(elem => elem.addEventListener("mouseover", function() {
-    hoverBlock(elem);
+    if (colorize() == "color"){
+        hoverColor(elem);
+    } else {
+        hoverBlack(elem);
+    }
 }))
 
-function hoverBlock(blockAttribute) {
+function hoverColor(blockAttribute) {
     blockAttribute.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()}`;
+}
+
+function hoverBlack(blockAttribute) {
+    blockAttribute.style.backgroundColor = "black";
+}
+
+function colorize() {
+    return document.getElementById("colorize").value;
+}
+
+function blockSize() {
+    return document.getElementById("blocksize").value;
 }
 
 function randomColor() {
